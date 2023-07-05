397f0443333f939e3dea2316b9c52613aa2d68dc
hitty <kya1982@gmail.com>
2023-07-05 15:34:33 +0300

    правка страницы контакты плашка реклама для Мнений

--- modules/opinions_predictions/conf_mapping.php
+++ modules/opinions_predictions/conf_mapping.php
@@ -1,156 +1,184 @@
 <?php
 return array(
     'opinions_predictions' => array(
-         'id' => array(
+        'id' => array(
             'type' => TYPE_INTEGER,
             'nodisplay' => true,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => true
-         )
-        ,'type' => array(
+        )
+    , 'type' => array(
             'type' => TYPE_INTEGER,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'select',
-			'values' => array(1=>'мнения',2=>'прогнозы',3=>'интервью'),
+            'values' => array(1 => 'мнения', 2 => 'прогнозы', 3 => 'интервью'),
             'label' => 'Тип записи',
             'tip' => 'Мнение или прогноз'
         )
-        ,'id_expert' => array(
+    , 'id_expert' => array(
             'type' => TYPE_INTEGER,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'select',
-			'values' => array(0=>'- выберите эксперта -'),
+            'values' => array(0 => '- выберите эксперта -'),
             'label' => 'Имя эксперта',
             'tip' => ''
         )
-        ,'id_estate_type' => array(
+    , 'id_estate_type' => array(
             'type' => TYPE_INTEGER,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'select',
-			'values' => array(0=>'- выберите тип -'),
+            'values' => array(0 => '- выберите тип -'),
             'label' => 'Тип недвижимости',
             'tip' => ''
         )
-        ,'author' => array(
+    , 'author' => array(
             'type' => TYPE_STRING,
             'max' => 255,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => false,
             'fieldtype' => 'text',
             'label' => 'Автор',
             'tip' => 'Автор'
-        ) 
-		,'annotation' => array(
+        )
+    , 'annotation' => array(
             'type' => TYPE_STRING,
             'max' => 100,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'textarea',
             'label' => 'Аннотация',
             'tip' => 'Короткий текст, не более 100 знаков'
-        )		
-		,'text' => array(
+        )
+    , 'text' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'textarea',
             'editor' => 'big',
             'label' => 'Текст',
             'tip' => 'Полное содержимое мнения/прогноза'
-        )		
-		,'date' => array(
+        )
+    , 'date' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'date',
             'label' => 'Дата размещения',
             'tip' => 'Дата размещения мнения/прогноза'
-        )		
+        )
+    , 'advert' => [
+            'type' => TYPE_INTEGER,
+            'min' => 1,
+            'max' => 2,
+            'allow_empty' => false,
+            'allow_null' => false,
+            'fieldtype' => 'radio',
+            'values' => [2 => 'Нет', 1 => 'Да'],
+            'label' => 'Показывать баннер "Реклама"',
+            'tip' => 'Платное агентство'
+        ]
+    , 'advert_url' => [
+            'type' => TYPE_STRING,
+            'max' => 100,
+            'allow_empty' => true,
+            'allow_null' => true,
+            'fieldtype' => 'text',
+            'label' => 'Ссылка на рекламодателя',
+            'tip' => 'обязательно наличие http'
+        ]
+    , 'token' => [
+            'type' => TYPE_STRING,
+            'max' => 100,
+            'allow_empty' => true,
+            'allow_null' => true,
+            'fieldtype' => 'text',
+            'label' => 'Токен',
+        ]
     ),
     'experts' => array(
-         'id' => array(
+        'id' => array(
             'type' => TYPE_INTEGER,
             'nodisplay' => true,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => true
-         )
-        ,'title' => array(
+        )
+    , 'title' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'text',
             'label' => 'ФИО',
             'tip' => 'ФИО эксперта'
         )
-        ,'company' => array(
+    , 'company' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'text',
             'label' => 'Должность',
             'tip' => 'Должность эксперта'
-        )					 
-        ,'id_agency' => array(
+        )
+    , 'id_agency' => array(
             'type' => TYPE_INTEGER,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'select',
-			'values' => array(0=>'- выберите агентство -'),
+            'values' => array(0 => '- выберите агентство -'),
             'label' => 'Агентство',
             'tip' => 'Выберите агентство для эксперта'
         )
-        ,'email' => array(
+    , 'email' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => true,
             'fieldtype' => 'text',
             'label' => 'E-mail',
             'tip' => ''
         )
-		,'bio' => array(
+    , 'bio' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => true,
             'fieldtype' => 'textarea',
-			'editor' => 'big',
+            'editor' => 'big',
             'label' => 'Биография',
             'tip' => 'Краткая биография'
-        )		
-    )	
-    ,'agencies' => array(
-         'id' => array(
+        )
+    )
+, 'agencies' => array(
+        'id' => array(
             'type' => TYPE_INTEGER,
             'nodisplay' => true,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => true
-         )
-        ,'title' => array(
+        )
+    , 'title' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'text',
             'label' => 'Название агентства',
             'tip' => 'Полное название агентства'
-        )	
-	)
-    ,'estate_types' => array(
-         'id' => array(
+        )
+    )
+, 'estate_types' => array(
+        'id' => array(
             'type' => TYPE_INTEGER,
             'nodisplay' => true,
-            'allow_empty' => true, 
+            'allow_empty' => true,
             'allow_null' => true
-         )
-        ,'title' => array(
+        )
+    , 'title' => array(
             'type' => TYPE_STRING,
-            'allow_empty' => false, 
+            'allow_empty' => false,
             'allow_null' => false,
             'fieldtype' => 'text',
             'label' => 'Название недвижимости',
             'tip' => 'Название типа недвижимости'
-        )	
-	)	
+        )
+    )
 );
 ?>
